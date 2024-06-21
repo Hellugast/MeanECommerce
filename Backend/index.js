b158a2a809144880ae209f6eaabcb03d7f02a565
Batu YURT <asdasd018@hotmail.com>
2024-06-21 23:39:39 +0300

    Auth router and register

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const authRouter = require("./routers/auth.router")
+app.use("/api/auth", authRouter);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("uygulama ayakta ve 5000 portuna istek bekliyor"))
 
