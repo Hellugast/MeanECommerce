9f2d0b018fa973a643ae0cbee5a0143395182efe
Batu YURT <asdasd018@hotmail.com>
2024-06-22 04:21:06 +0300

    Layout structure created

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterModule],
+  template: "<router-outlet></router-outlet>",
 })
 export class AppComponent {
   title = 'Frontend';
